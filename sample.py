9ef451b3d13baf58172678c791fbba8f8ca75d91
anju <anjali93ravindran@gmail.com>
2022-03-08 20:39:22 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,128 @@
+#print("hello world")
+#print("hello anjali")
+
+#if 5>2:
+# print("reater")
+# print("Five is greater than two!") 
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+#x,y,z="string",4,4.0
+#print(x)
+#print(y)
+#print(z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
+
+#fruits=["apple","banana","cherry"]
+#x, y, z = fruits
+#print(x)
+#print(y)
+#print(z)
+
+#x = "Python is "
+#y = "awesome"
+#z =  x + y
+#print(z)
+#print(x+  y)
+
+#x = "awesome"
+#def myfunc():
+#  x = "fantastic"
+#  print("Python is " + x)
+#myfunc()
+#print("Python is " + x) 
+
+
+
+#x = range(6)
+#x = {"name" : "John", "age" : 36}
+#x = {"apple", "banana", "cherry"}
+#x = frozenset({"apple", "banana", "cherry"})
+#x = b"Hello"
+#x = bytearray(5)
+#x = memoryview(bytes(5))
+#print(x)
+#print(type(x))
+
+#x = list(("apple", "banana", "cherry"))
+#print(x)
+#print(type(x))
+#
+
+#import random
+#
+#print(random.randrange(1, 10)) 
+
+#a ="""Lorem ipsum dolor sit amet,
+#consectetur adipiscing elit,
+#sed do eiusmod tempor incididunt
+#ut labore et dolore magna aliqua."""
+#print(a) 
+
+#for x in "banana":
+#  print(x)
+
+
+#txt = "The best things in life are free!"
+#print("tree" in txt)
+
+#txt = "The best things in life are free!"
+#if "free" in txt:
+#  print("Yes, 'free' is present.")
+
+#txt = "The best things in life are free!"
+#if "expensive" not in txt:
+#  print("No, 'expensive' is NOT present.")
+#
+#b = "Hello, World!"
+#print(b[2:5])     #slicing
+#
+
+#b = "Hello, World!"
+#print(b[2:])
+#b = "Hello, World!"
+#print(b[-5:-2])
+#
+#
+#a = "Hello, World!"
+#print(a.upper())
+#
+#a = " Hello, World! "
+#print(a.strip()) # returns "Hello, World!" 
+
+#a = "Hello, World!"
+#print(a.replace("H", "J"))
+
+#a = "Hello, World!"
+#print(a.split(",")) # returns ['Hello', ' World!'] 
+#
+#a = "Hello"
+#b = "World"
+#c = a + " " + b
+#print(c)
+#
+#age = 36
+#txt = "My name is John, and I am {}"
+#print(txt.format(age))
+#
+#quantity = 3
+#itemno = 567
+#price = 49.95
+#myorder = "I want {} pieces of item {} for {} dollars."
+#print(myorder.format(quantity, itemno, price))
+
+#txt = "We are the so-called \"Vikings\" from the north."  #escape characters
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
+#this is to show that it is a modified file
